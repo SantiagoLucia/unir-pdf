2878166fbf4b42c69075130c86083819ac5683d0
SantiLP <34819955+SantiagoLucia@users.noreply.github.com>
2024-09-09 23:51:20 -0300

    Refactor

--- main.py
+++ main.py
@@ -1,38 +1,50 @@
 import PyPDF2
 from pathlib import Path
 import os
-from tkinter import filedialog
+from tkinter import filedialog, Tk
 
+def unir_pdf(lista_pdf: tuple) -> None:
+    """
+    Unir múltiples archivos PDF en un solo archivo PDF.
 
-def unir_pdf(lista_pdf: tuple) -> None: 
-    pdf_merge = PyPDF2.PdfMerger()
+    Args:
+        lista_pdf (tuple): Una tupla que contiene las rutas de los archivos PDF a unir.
+    """
+    pdf_merger = PyPDF2.PdfMerger()
     for filename in lista_pdf:
         with open(filename, "rb") as pdf_file:
             pdf_reader = PyPDF2.PdfReader(pdf_file)
-            pdf_merge.append(pdf_reader)
+            pdf_merger.append(pdf_reader)
 
     resultado = filedialog.asksaveasfilename(
-        defaultextension="pdf",
+        defaultextension=".pdf",
         confirmoverwrite=True,
-        filetypes=[("PDF files", "*.pdf")],
+        filetypes=[("Archivos PDF", "*.pdf")],
         initialdir=Path.home() / "Escritorio",
-        )
-    
-    if resultado:
-        pdf_merge.write(resultado)    
-        try:
-            os.startfile(resultado)    
-        except:
-            pass
+    )
 
- 
+    if resultado:
+        with open(resultado, "wb") as output_pdf:
+            pdf_merger.write(output_pdf)
+        try:
+            os.startfile(resultado)
+        except Exception as e:
+            print(f"No se pudo abrir el archivo: {e}")
+
 def main() -> None:
+    """
+    Función principal para seleccionar archivos PDF y unirlos.
+    """
+    # Ocultar la ventana raíz de Tkinter
+    root = Tk()
+    root.withdraw()
+
     filenames = filedialog.askopenfilenames(
-        filetypes=[("PDF files", "*.pdf")],
-        initialdir=Path.home() / "Downloads"                 
-        )
+        filetypes=[("Archivos PDF", "*.pdf")],
+        initialdir=Path.home() / "Downloads"
+    )
     if filenames:
         unir_pdf(filenames)
-        
+
 if __name__ == "__main__":
     main()
